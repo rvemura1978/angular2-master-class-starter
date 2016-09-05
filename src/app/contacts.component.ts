e3c1c9999a759a9868a05e456b1fb1f8f5e80e4b
Radhika Vemura <Radhika Vemura>
2016-09-05 09:26:26 +0100

    interface

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ContactsHeaderComponent } from './contacts-header/contacts-header.component';
+import { Contact} from './models/contact';
 
 @Component({
   selector: 'trm-contacts-app',
   templateUrl: 'contacts.component.html',
-  styleUrls: ['contacts.component.css']
+  styleUrls: ['contacts.component.css'],
+  directives: [ ContactsHeaderComponent]
 })
 export class ContactsAppComponent {
   title = 'Angular 2 Master Class setup works!';
+  contact: Contact = {
+    id: 7,
+    name: 'Diana Ellis',
+    email: '',
+    phone: '',
+    birthday: '',
+    website: '',
+    image: '/assets/images/6.jpg',
+    address: {
+      street: '6554 park lane',
+      zip: '43378',
+      city: 'Rush',
+      country: 'United States'
+    }
+  };
 }
