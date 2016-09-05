d669ba9e734e330d9b0e6715e2ad51e83badf971
rvemura1978 <rvemura.net@gmail.com>
2016-09-05 10:37:52 +0100

    exercise-5_introduce-contactsservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 import { ContactsAppComponent } from './contacts.component';
 import { ContactsHeaderComponent } from './contacts-header/contacts-header.component';
 
+import {ContactsServiceService} from './contacts-service/contacts-service.service';
+
 @NgModule({
   declarations: [ContactsAppComponent, ContactsHeaderComponent],
   imports: [BrowserModule],
+  providers:[ContactsServiceService],
   bootstrap: [ContactsAppComponent]
 })
 export class ContactsModule {
